5e3fb38aa0f29a5ce3e006e31690d7198e6bdaea
Anderson Veeck <andersonveeck@gmail.com>
2022-07-10 02:24:55 -0300

    ajustes no index e correção do header logado

--- public/index.js
+++ public/index.js
@@ -34,93 +34,119 @@ $(document).ready(function () {
       // loggedIn = false
     }
   });
+
+  showLoginPopup()
+  loginPopup()
+  hideLoginPopup()
+  hideLoginResultPopup()
+  showPasswordRecoveryPopup()
+  hidePasswordRecoveryPopup()
+  hidePassowordRecoveryResultPopup()
+  passwordReset()
+  logout()
 });
 
 // LOGOFF SYSTEM
-var btnSair = document.querySelector('#btnSair');
-
-btnSair.addEventListener('click', function (event) {
-  logoff();
-});
+function logout() {
+  var btnSair = document.querySelector('#btnSair');
+  
+  btnSair.addEventListener('click', function (event) {
+    logoff();
+  });
+}
 
 var isPasswordPopupActive = false;
 
 // Exibir login-popup
-document.querySelector('#show-login').addEventListener('click', function () {
-  if (isPasswordPopupActive == false) {
-    document.querySelector('.popup').classList.add('active');
-  } else {
-    document.querySelector('.popup.password-recovery').classList.remove('active');
-    isPasswordPopupActive = false;
-  }
-});
+function showLoginPopup() {
+  document.querySelector('#show-login').addEventListener('click', function () {
+    if (isPasswordPopupActive == false) {
+      document.querySelector('.popup').classList.add('active');
+    } else {
+      document.querySelector('.popup.password-recovery').classList.remove('active');
+      isPasswordPopupActive = false;
+    }
+  });
+}
 
 // Esconder login-popup
-document.querySelector('.popup .close-btn').addEventListener('click', function () {
-  document.querySelector('#email').value = '';
-  document.querySelector('#password').value = '';
-  document.querySelector('.popup').classList.remove('active');
-});
+function hideLoginPopup() {
+  document.querySelector('.popup .close-btn').addEventListener('click', function () {
+    document.querySelector('#email').value = '';
+    document.querySelector('#password').value = '';
+    document.querySelector('.popup').classList.remove('active');
+  });
+}
 
-// // Esconder login-result-popup
-document.querySelector('.popup.login-result .close-btn').addEventListener('click', function () {
-  document.querySelector('.popup.login-result').classList.remove('active');
-  document.querySelector('#login-result-msg').innerHTML = '';
-});
+// Esconder login-result-popup
+function hideLoginResultPopup() {
+  document.querySelector('.popup.login-result .close-btn').addEventListener('click', function () {
+    document.querySelector('.popup.login-result').classList.remove('active');
+    document.querySelector('#login-result-msg').innerHTML = '';
+  });
+}
 
 // Exibir password-recovery-popup
-document.querySelector('#forgot-password').addEventListener('click', function () {
-  document.querySelector('#email').value = '';
-  document.querySelector('#password').value = '';
-  document.querySelector('.popup').classList.remove('active');
-  document.querySelector('.popup.password-recovery').classList.add('active');
-  isPasswordPopupActive = true;
-});
+function showPasswordRecoveryPopup() {
+  document.querySelector('#forgot-password').addEventListener('click', function () {
+    document.querySelector('#email').value = '';
+    document.querySelector('#password').value = '';
+    document.querySelector('.popup').classList.remove('active');
+    document.querySelector('.popup.password-recovery').classList.add('active');
+    isPasswordPopupActive = true;
+  });
+}
 
 // Esconder password-recovery-popup
-
-document.querySelector('.popup.password-recovery .close-btn').addEventListener('click', function () {
-  document.querySelector('#recovery-email').value = '';
-  document.querySelector('.popup.password-recovery').classList.remove('active');
-  isPasswordPopupActive = false;
-});
+function hidePasswordRecoveryPopup() {
+  document.querySelector('.popup.password-recovery .close-btn').addEventListener('click', function () {
+    document.querySelector('#recovery-email').value = '';
+    document.querySelector('.popup.password-recovery').classList.remove('active');
+    isPasswordPopupActive = false;
+  });
+}
 
 // Esconder password-recovery-result-popup
-document.querySelector('.popup.password-recovery-result .close-btn').addEventListener('click', function () {
-  document.querySelector('#recovery-result-msg').innerHTML = '';
-  document.querySelector('.popup.password-recovery-result').classList.remove('active');
-});
+function hidePassowordRecoveryResultPopup() {
+  document.querySelector('.popup.password-recovery-result .close-btn').addEventListener('click', function () {
+    document.querySelector('#recovery-result-msg').innerHTML = '';
+    document.querySelector('.popup.password-recovery-result').classList.remove('active');
+  });
+}
 
 // AUTHENTICATION SYSTEM
-
-var btnEntrar = document.querySelector('#btnEntrar');
-// var spanUserInfo = document.querySelector("#userInfo");
-
-btnEntrar.addEventListener('click', function (event) {
-  event.preventDefault();
-  const formData = {
-    email: document.querySelector('#email').value,
-    senha: document.querySelector('#password').value
-  };
-  // console.log(formData);
-  login(formData);
-});
+function loginPopup() {
+  var btnEntrar = document.querySelector('#btnEntrar');
+  // var spanUserInfo = document.querySelector("#userInfo");
+  
+  btnEntrar.addEventListener('click', function (event) {
+    event.preventDefault();
+    const formData = {
+      email: document.querySelector('#email').value,
+      senha: document.querySelector('#password').value
+    };
+    // console.log(formData);
+    login(formData);
+  });
+}
 
 // RESET PASSWORD
+function passwordReset() {
+  var sendRecovery = document.querySelector('#btn-send-recovery');
+  sendRecovery.addEventListener('click', function (event) {
+    // event.preventDefault();
+    const email = document.querySelector('#recovery-email').value;
+  
+    // console.log(email);
+    passwordRecovery(email);
+  });
+}
 
-var sendRecovery = document.querySelector('#btn-send-recovery');
-
-sendRecovery.addEventListener('click', function (event) {
-  // event.preventDefault();
-  const email = document.querySelector('#recovery-email').value;
-
-  // console.log(email);
-  passwordRecovery(email);
-});
 
 function changeHeaderStyle () {
   $('.header__links').hide();
   $('.header__links-loggedIn').show();
+  $('.header__links-loggedIn').css('display', 'flex');
 
   console.log('entrei no changeHeaedr');
 
